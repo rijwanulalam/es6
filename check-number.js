e3e600d00f4f37af9f6b10e9e78ca28ba6ea6436
rijwanulalam <rijwanul007@gmail.com>
2023-11-25 19:37:32 +0600

    first commit

---
+++ check-number.js
@@ -0,0 +1,8 @@
+const checkNumber = num =>{
+    if(typeof num === 'number'){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+}
+checkNumber('-5');
---
+++ truthy-falsy.js
@@ -0,0 +1,11 @@
+// if([] == false){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
+
+if(true === 'true'){
+    console.log("true")
+}else{
+    console.log("false")
+}
